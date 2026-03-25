73fd80b629aa04f8edabb65251840d4d15f73039
Saliva-sys <aweidlichova@gmail.com>
2026-03-26 00:27:46 +0100

    feat: implement rating logic using React useState and map

--- src/RatingComponent.jsx
+++ src/RatingComponent.jsx
@@ -16,75 +16,90 @@ const RatingComponent = () => {
   }, []); // [] znamená, že sa to zmení len pri otvorení tohto komponentu
 
   return (
-    <div className="rating-container">
-      <div className="rating__card">
-        {/* Rating state start */}
-        <div className="star__symbol">
-          <img src="./images/icon-star.svg" className="img__star" alt="star" aria-hidden="true" />
-        </div>
-        <h1 className="rating__card-title">How did we do?</h1>
-        <p className="rating__card-text">
-          Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
-        </p>
-      </div>
-      <div className="rating__card-button">
-        <div className="rating__btn">
-          {ratings.map((number) => ( // prejdeme vsetky cisla v poli "ratings" a pre kazde cislo vytvorime tlacidlo, ktore bude mat text rovnaky ako to cislo, a ked klikneme na tlacidlo, tak sa nam do krabicky "selectedRating" ulozi to cislo, ktore sme klikli
-          <button
-            key={number} // react potrebuje vediet, ktore cislo je ktore, aby vedel, ktore ma prekreslit, ked nan klikneme, akasi menovka pre cislo, aby ju vedel identifikovat
-            type="button"
-            className={`btn ${selectedRating === number ? 'active' : ''}`} // ak je cislo, ktore sme klikli rovnake ako cislo, ktore je v krabicke "selectedRating", tak sa tlacidlo zafarbí (dostane class "active"), inak bude bez farby. React sa opýta každého tlačidla: „Rovnáš sa ty číslu 3?“ To, ktoré povie „Áno“, dostane triedu active
-            onClick={() => setSelectedRating(number)}>
-              {number}
-            </button>
-          ))}
-          
-        </div>
-        <button 
-          className="submit__btn" 
-          type="button"
-            onClick={() => {
-              // ak je nieco v krabicke
-              if (selectedRating !== null) {
-                setIsSubmitted(true); // prepneme krabicku isSubmitted z "false" na "true"
-              } else {
-                alert("Please select a rating first!") // prida upozornenie, ak chce niekto kliknut "submit" bez zvoleneho cisla
-              }              
-            }}>
-            SUBMIT
-            </button>
-        {/* <!-- Rating state end --> */}
-      </div>
+    <main>
+      <div className="rating-container">
+        { !isSubmitted ? (
+        <> 
+          <div className="rating__card">
+            {/* Rating state start */}
+            <div className="star__symbol">
+              <img src="./images/icon-star.svg" className="img__star" alt="star" aria-hidden="true" />
+            </div>
+            <h1 className="rating__card-title">How did we do?</h1>
+            <p className="rating__card-text">
+              Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+            </p>
+          </div>
 
-      <div className="thank__you-card hidden">
-        {/* <!-- Thank you state start --> */}
-        <img src="./images/illustration-thank-you.svg" className="thank__you-img" alt="thank you" />
-        <p className="thank__you-selection">
-          You selected
-          <span className="user__rating">4</span>
-          {/* <!-- Add rating here --> */}
-          out of 5</p>
-        <button className="thank__you-thanks" type="button">
-          Thank you!
-        </button>
-        <p className="thank__you-text">
-          We appreciate you taking the time to give a rating. If you ever need more support, don&rsquo;t hesitate to get in touch!
-        </p>
-        {/* <!-- Thank you state end --> */}
+          <div className="rating__card-button">
+            <div className="rating__btn">
+              {ratings.map((number) => ( // prejdeme vsetky cisla v poli "ratings" a pre kazde cislo vytvorime tlacidlo, ktore bude mat text rovnaky ako to cislo, a ked klikneme na tlacidlo, tak sa nam do krabicky "selectedRating" ulozi to cislo, ktore sme klikli
+              
+              <button
+                key={number} // react potrebuje vediet, ktore cislo je ktore, aby vedel, ktore ma prekreslit, ked nan klikneme, akasi menovka pre cislo, aby ju vedel identifikovat
+                type="button"
+                className={`btn ${selectedRating === number ? 'active' : ''}`} // ak je cislo, ktore sme klikli rovnake ako cislo, ktore je v krabicke "selectedRating", tak sa tlacidlo zafarbí (dostane class "active"), inak bude bez farby. React sa opýta každého tlačidla: „Rovnáš sa ty číslu 3?“ To, ktoré povie „Áno“, dostane triedu active
+                onClick={() => setSelectedRating(number)}>
+                  {number}
+              </button>
+                  )
+                )
+              }             
+            </div>
+                    
+            <button 
+              className="submit__btn" 
+              type="button"
+                onClick={() => {
+                  // ak je nieco v krabicke
+                  if (selectedRating !== null) {
+                    setIsSubmitted(true); // prepneme krabicku isSubmitted z "false" na "true"
+                  } else {
+                    alert("Please select a rating first!") // prida upozornenie, ak chce niekto kliknut "submit" bez zvoleneho cisla
+                  }              
+                }}>
+                SUBMIT
+            </button>
+          {/* <!-- Rating state end --> */}
+          </div>
+        </>
+
+        ) : (
+
+        <div className="thank__you-card">
+          {/* <!-- Thank you state start --> */}
+          <img src="./images/illustration-thank-you.svg" className="thank__you-img" alt="thank you" />
+          <p className="thank__you-selection">You selected <span className="user__rating">{selectedRating}</span> out of 5</p>
+            {/* <!-- Add rating here --> */}
+            
+          <button 
+            className="thank__you-thanks" 
+            type="button"
+            onClick={() => {
+              setIsSubmitted(false);    // 1. Povieme: "Ešte nie je odoslané" (výhybka sa prepne späť)
+              setSelectedRating(null);  // 2. Vymažeme vybrané číslo (krabička bude znova prázdna)
+            }}
+          >
+            Thank You
+          </button>
+          <p className="thank__you-text">
+            We appreciate you taking the time to give a rating. If you ever need more support, don&rsquo;t hesitate to get in touch!
+          </p>
+          {/* <!-- Thank you state end --> */}          
+        </div>
+        )}
       </div>
 
       <footer className="attribution">
         Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" className="attribution-link" target="_blank" rel="noopener noreferrer">
-          Frontend Mentor
-        </a>
+          Frontend Mentor</a>
         . Coded by
         <a href="https://github.com/Saliva-sys" className="attribution-link" target="_blank" rel="noopener noreferrer">
-          Adriana Weidlichova
-        </a>
+          Adriana Weidlichova</a>
         .
-      </footer>
-    </div>
+      </footer>      
+    </main>
   );
 };
 
